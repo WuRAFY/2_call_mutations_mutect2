73592f0546dcaa497afdea3525fe345d1b560b24
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:42:21 +0800

    Update pon.sh

--- pon.sh
+++ pon.sh
@@ -6,9 +6,9 @@
 #PBS -o pon.log
 #PBS -e pon.log
 
-workdir=.
+workdir=./mutect2
 cd $workdir
 module load gatk/4.1.3.0
 
 ulimit -n 4000
-gatk Mutect2 -R ./ref/reference.fasta -L ./resource/test.bed -I ../data/${1}_normal.bam -max-mnp-distance 0 -O ${1}_N.vcf.gz
+gatk Mutect2 -R ../ref/reference.fasta -L ../resources/test.bed -I ../data/${1}_normal.bam -max-mnp-distance 0 -O ${1}_N.vcf.gz
