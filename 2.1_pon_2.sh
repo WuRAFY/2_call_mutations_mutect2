2410e24ad620adf7e011e5388c3e48d0c30302b1
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:43:58 +0800

    Update 2.1_pon_2.sh

--- 2.1_pon_2.sh
+++ 2.1_pon_2.sh
@@ -3,10 +3,10 @@
 #PBS -l mem=20g
 #PBS -q large
 #PBS -V
-#PBS -o /data/liuxd/21summer_training/workdir/wufy/out.log
-#PBS -e /data/liuxd/21summer_training/workdir/wufy/error.log
+#PBS -o out.log
+#PBS -e error.log
 
-workdir=/data/liuxd/21summer_training/workdir/wufy/mutect2
+workdir=./mutect2
 cd $workdir
 module load gatk/4.1.3.0
 
@@ -15,7 +15,5 @@ for n in $(cat ../pat_list.txt)
 do
 val=${val}-V" "${n}_N.vcf.gz" "
 done
-echo $val
-gatk GenomicsDBImport --merge-input-intervals true --genomicsdb-workspace-path pon_db --reader-threads 8 -L /data/liuxd/21summer_training/data/input/bedfile/truseq/truseq.bed -R ../ref/Homo_sapiens_assembly38.fasta $val
-gatk CreateSomaticPanelOfNormals -R ../ref/Homo_sapiens_assembly38.fasta -V gendb://pon_db -O pon.vcf.gz
-
+gatk GenomicsDBImport --merge-input-intervals true --genomicsdb-workspace-path pon_db --reader-threads 8 -L ../resources/test.bed -R ../ref/reference.fasta $val
+gatk CreateSomaticPanelOfNormals -R ../ref/reference.fasta -V gendb://pon_db -O pon.vcf.gz
