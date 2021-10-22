2781f6f390a8c15b013b4baa0ca85738de728165
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:41:15 +0800

    Update pon.sh

--- pon.sh
+++ pon.sh
@@ -3,12 +3,12 @@
 #PBS -l mem=8g
 #PBS -q medium
 #PBS -V
-#PBS -o /data/liuxd/21summer_training/workdir/wufy/out_pon.log
-#PBS -e /data/liuxd/21summer_training/workdir/wufy/error_pon.log
+#PBS -o pon.log
+#PBS -e pon.log
 
-workdir=/data/liuxd/21summer_training/workdir/wufy/mutect2
+workdir=.
 cd $workdir
 module load gatk/4.1.3.0
 
 ulimit -n 4000
-gatk Mutect2 -R ../ref/Homo_sapiens_assembly38.fasta -L /data/liuxd/21summer_training/data/input/bedfile/truseq/truseq.bed -I ../data/${1}_N_MD_BQSR.bam -max-mnp-distance 0 -O ${1}_N.vcf.gz
+gatk Mutect2 -R ./ref/reference.fasta -L ./resource/test.bed -I ../data/${1}_normal.bam -max-mnp-distance 0 -O ${1}_N.vcf.gz
