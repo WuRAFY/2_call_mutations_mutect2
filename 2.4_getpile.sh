3348fa4011d5606808a721ce9058a5d6af894814
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:46:35 +0800

    Update 2.4_getpile.sh

--- 2.4_getpile.sh
+++ 2.4_getpile.sh
@@ -3,18 +3,17 @@
 #PBS -l mem=8g
 #PBS -q large
 #PBS -V
-#PBS -o /data/liuxd/21summer_training/workdir/wufy/out.log
-#PBS -e /data/liuxd/21summer_training/workdir/wufy/error.log
+#PBS -o out.log
+#PBS -e error.log
 
-workdir=/data/liuxd/21summer_training/workdir/wufy/mutect2
+workdir=./mutect2
 cd $workdir
 
 sed '1d' ../sample_list.txt|awk -vFPAT="([^,]+)|(\"[^\"]+\")" '{print $27}'|while read line
 do
 gatk GetPileupSummaries \
--I ../data/${line}_sort_MD_BQSR.bam \
--V /data/liuxd/21summer_training/data/input/af-only-gnomad.hg38.vcf.gz \
--L /project/liuxd/npc/hg38/input/bedfile/truseq/truseq.bed \
+-I ../data/${line}_tumor.bam \
+-V ../resources/germline_resource.vcf.gz \
+-L ../resources/test.bed \
 -O ${line}_pile.table
 done
-
