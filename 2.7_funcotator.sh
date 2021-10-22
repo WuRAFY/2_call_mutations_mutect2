0803c948be0bc6d9afff5a86912d94bb66327fbd
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:52:29 +0800

    Update 2.7_funcotator.sh

--- 2.7_funcotator.sh
+++ 2.7_funcotator.sh
@@ -3,20 +3,20 @@
 #PBS -l mem=8g
 #PBS -q small
 #PBS -V
-#PBS -o /data/liuxd/21summer_training/workdir/wufy/out.log
-#PBS -e /data/liuxd/21summer_training/workdir/wufy/error.log
+#PBS -o out.log
+#PBS -e error.log
 
 module load gatk/4.1.3.0
 #gatk FuncotatorDataSourceDownloader --somatic --validate-integrity --extract-after-download
-workdir=/data/liuxd/21summer_training/workdir/wufy/mutect2
+workdir=./mutect2
 cd $workdir
 cat ../pat_list.txt|while read line
 do
 gatk Funcotator \
 --variant ${line}_filter.vcf \
---reference ../ref/Homo_sapiens_assembly38.fasta \
+--reference ../ref/reference.fasta \
 --ref-version hg38 \
---data-sources-path /home/wufy/software/funcotator_dataSources.v1.6.20190124s \
+--data-sources-path ../resoureces/funcotator_dataSources.v1.6.20190124s \
 --output ${line}.maf \
 --remove-filtered-variants \
 --output-file-format MAF \
