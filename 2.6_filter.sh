2d90b2ac2ce06262e022b7094c65d2645702f949
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:51:23 +0800

    Update 2.6_filter.sh

--- 2.6_filter.sh
+++ 2.6_filter.sh
@@ -3,14 +3,14 @@
 #PBS -l mem=8g
 #PBS -q medium
 #PBS -V
-#PBS -o /data/liuxd/21summer_training/workdir/wufy/out.log
-#PBS -e /data/liuxd/21summer_training/workdir/wufy/error.log
+#PBS -o out.log
+#PBS -e error.log
 
-workdir=/data/liuxd/21summer_training/workdir/wufy/mutect2
+workdir=./mutect2
 cd $workdir
 cat ../pat_list.txt|while read line
 do
-gatk FilterMutectCalls -R ../ref/Homo_sapiens_assembly38.fasta \
+gatk FilterMutectCalls -R ../ref/reference.fasta \
 -V ${line}_T.vcf.gz \
 --contamination-table ${line}_contamination.table \
 --tumor-segmentation ${line}_segment.table \
