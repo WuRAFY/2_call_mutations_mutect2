b3a0d9e541690493cfced70412d6609456c5a571
WuRAFY <64182733+WuRAFY@users.noreply.github.com>
2021-10-22 11:54:08 +0800

    Update mutect2.sh

--- mutect2.sh
+++ mutect2.sh
@@ -3,20 +3,20 @@
 #PBS -l mem=4g
 #PBS -q large
 #PBS -V
-#PBS -o /data/liuxd/21summer_training/workdir/wufy/out.log
-#PBS -e /data/liuxd/21summer_training/workdir/wufy/error.log
+#PBS -o out.log
+#PBS -e error.log
 
 ulimit -n 4000
-workdir=/data/liuxd/21summer_training/workdir/wufy/mutect2
+workdir=./mutect2
 cd $workdir
 module load gatk/4.1.3.0
-gatk Mutect2 -R ../ref/Homo_sapiens_assembly38.fasta \
--I ../data/${1}_T_sort_MD_BQSR.bam \
--I ../data/${1}_N_sort_MD_BQSR.bam \
--L /data/liuxd/21summer_training/data/input/bedfile/truseq/truseq.bed \
+gatk Mutect2 -R ../ref/reference.fasta \
+-I ../data/${1}_tumor.bam \
+-I ../data/${1}_normal.bam \
+-L ../resources/truseq.bed \
 -normal ${1}_N \
---germline-resource /data/liuxd/21summer_training/data/input/af-only-gnomad.hg38.vcf.gz \
---panel-of-normals /data/liuxd/21summer_training/data/input/1000g_pon.hg38.vcf.gz \
+--germline-resource ../resources/germline_resouce.vcf.gz \
+--panel-of-normals pon.vcf.gz \
 --f1r2-tar-gz ${1}_f1r2.tar.gz \
 -O ${1}_T.vcf.gz
 
